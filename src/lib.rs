8ba266f9b32c62dbbd94827b7419f514ed375739
jozz024 <74272560+jozz024@users.noreply.github.com>
2023-01-23 18:29:37 -0600

    add the ability to set args

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use nn::os::FiberFunction;
 use nn::os::FiberType;
 use std::boxed::Box;
 
-fn construct_fiber(function: FiberFunction ) -> FiberType {
+fn construct_fiber(function: FiberFunction, args: *const u8 ) -> FiberType {
     // Constructing the nn::os::FiberType struct with default values (besides function, because it requires a `FiberFunction`).
     let mut fiber = nn::os::FiberType {
         status: 0,
@@ -21,15 +21,19 @@ fn construct_fiber(function: FiberFunction ) -> FiberType {
     // Leak the stack, so we get a mutable reference.
     let stack = Box::leak(stack);
     // Run nn::os::InitializeFiber with the required arguments to setup the current fiber.
-    unsafe { nn::os::InitializeFiber(&mut fiber, function, std::ptr::null_mut(), stack.as_ptr() as *mut skyline::libc::c_void, 0x10000, 0) };
+    unsafe { nn::os::InitializeFiber(&mut fiber, function, args as *mut skyline::libc::c_void, stack.as_ptr() as *mut skyline::libc::c_void, 0x10000, 0) };
     // Return an instance of the nn::os::FiberType struct.
     return fiber;
 }
 
 extern "C" fn test(args: *const u8) -> *const FiberType {
     println!("First test function running!");
-    // We construct the fiber and then box it so we get a mutable reference (i think?)
-    let fiber = Box::new(construct_fiber(test2));
+    // Print the value of `args`, in this case it will be 111.
+    dbg!(unsafe { *args });
+
+    // We construct the fiber and then box it so we get a mutable reference
+    // Set args to a null pointer because we dont use them for `test2`
+    let fiber = Box::new(construct_fiber(test2, std::ptr::null()));
     let fiber = Box::leak(fiber);
     // Note: Switching to the next fiber is crucial if you return a fiber, or else the game will crash.
     unsafe{ nn::os::SwitchToFiber(fiber) }
@@ -37,14 +41,18 @@ extern "C" fn test(args: *const u8) -> *const FiberType {
     return fiber;
 }
 
-extern "C" fn test2(args: *const u8) -> *const FiberType {
+extern "C" fn test2(_args: *const u8) -> *const FiberType {
     println!("Second Test Function running!");
-    // Return a null pointer to go back to executing the `main()` function, or whatever function the fiber started in
+    // Return a null pointer to go back to executing the `main` function, or whatever function the fiber started in
     return std::ptr::null();
 }
 
 #[skyline::main(name = "fiber-bindings-test")]
 pub fn main() {
-    let mut fiber = construct_fiber(test);
+    // Construct the arguments for the function `test`, which is just an unsigned integer.
+    let args = Box::new(111);
+    let args = Box::leak(args);
+
+    let mut fiber = construct_fiber(test, args);
     unsafe { nn::os::SwitchToFiber(&mut fiber);}
 }
